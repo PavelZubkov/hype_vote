faa5f18bd4f80a18f1b3a29ac7b4e94c5c3cf5a7
PavelZubkov <PavelZubkov@users.noreply.github.com>
2024-11-23 07:21:47 +0000

    Deploying to gh-pages from @ PavelZubkov/hype_vote@d4358f7e714c3272b70b4a22a4da73b95ba5fc48 🚀

--- web.js
+++ web.js
@@ -8874,8 +8874,8 @@ var $;
 		dom_name(){
 			return "article";
 		}
-		field(){
-			return {...(super.field()), "tabIndex": (this.tabindex())};
+		attr(){
+			return {...(super.attr()), "tabIndex": (this.tabindex())};
 		}
 		sub(){
 			return [
