28809b3078c6058144a2ffbcd44f7eb8708ccdd1
PavelZubkov <PavelZubkov@users.noreply.github.com>
2024-12-29 07:18:42 +0000

    Deploying to gh-pages from @ PavelZubkov/hype_vote@d4358f7e714c3272b70b4a22a4da73b95ba5fc48 🚀

--- node.mjs
+++ node.mjs
@@ -9695,7 +9695,7 @@ var $;
             Tools: {
                 flex: {
                     basis: 'auto',
-                    grow: 1000,
+                    grow: 0,
                     shrink: 1,
                 },
                 display: 'flex',
