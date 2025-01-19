2c9aad60b9c1a8762ac1e8b40084d7ce830340c3
PavelZubkov <PavelZubkov@users.noreply.github.com>
2025-01-19 07:18:14 +0000

    Deploying to gh-pages from @ PavelZubkov/hype_vote@d4358f7e714c3272b70b4a22a4da73b95ba5fc48 🚀

--- node.js
+++ node.js
@@ -5119,6 +5119,7 @@ var $;
         static go(next) {
             this.href(this.link(next));
         }
+        static commit() { }
         constructor(prefix = '') {
             super();
             this.prefix = prefix;
--- node.test.js
+++ node.test.js
@@ -5110,6 +5110,7 @@ var $;
         static go(next) {
             this.href(this.link(next));
         }
+        static commit() { }
         constructor(prefix = '') {
             super();
             this.prefix = prefix;
